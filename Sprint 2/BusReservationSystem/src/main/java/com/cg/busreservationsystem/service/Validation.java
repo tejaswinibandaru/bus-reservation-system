869fef0905465c70e4bff8ae4bdce8b7d0738f25
MayankRaj2 <mayank2497@gmail.com>
2019-09-13 02:28:55 +0530

    validation in progress

--- Sprint 2/BusReservationSystem/src/main/java/com/cg/busreservationsystem/service/Validation.java
+++ Sprint 2/BusReservationSystem/src/main/java/com/cg/busreservationsystem/service/Validation.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 import com.cg.busreservationsystem.exception.BusException;
 import com.cg.busreservationsystem.exception.DateException;
 import com.cg.busreservationsystem.exception.PassengerException;
+import com.cg.busreservationsystem.ui.MyApplication;
 
 public class Validation {
 	
@@ -51,5 +52,18 @@ public class Validation {
 			throw new InputMismatchException("Wrong cost type entered");
 		}
 	}
+	
+	public static int checkInputChoice() throws InputMismatchException{
+		Scanner sc=new Scanner(System.in);
+		
+		try {
+			return sc.nextInt();
+		}catch (InputMismatchException e) {
+			// TODO: handle exception
+			throw new InputMismatchException("Wrong input choice");
+		}
+
+	}
+
 
 }
