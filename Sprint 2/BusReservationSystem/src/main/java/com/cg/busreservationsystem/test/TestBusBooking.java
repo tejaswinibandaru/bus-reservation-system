c6e2f23d3b0072156ae818f388f67e9b4647b6b5
Bandaru Tejaswini <bandaruteju@gmail.com>
2019-09-16 18:11:54 +0530

    Testing getting done

--- Sprint 2/BusReservationSystem/src/main/java/com/cg/busreservationsystem/test/TestBusBooking.java
+++ Sprint 2/BusReservationSystem/src/main/java/com/cg/busreservationsystem/test/TestBusBooking.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -18,7 +19,10 @@ import org.apache.log4j.PropertyConfigurator;
 
 import com.cg.busreservationsystem.dao.UserDao;
 import com.cg.busreservationsystem.dao.UserDaoImpl;
+import com.cg.busreservationsystem.dto.Booking;
 import com.cg.busreservationsystem.dto.Bus;
+import com.cg.busreservationsystem.dto.Passenger;
+import com.cg.busreservationsystem.dto.User;
 import com.cg.busreservationsystem.exception.BookingException;
 import com.cg.busreservationsystem.util.DBUtil;
 
@@ -72,6 +76,35 @@ public class TestBusBooking {
 		b.setDestination("pune");
 		b.setCost(500.0);
 		System.out.println(b);
+		
+		User user=new User();
+		user.setUsername("abcd#123");
+		user.setPassword("12345");
+		user.setUserType('C');
+		user.setEmail("abcd@gmail.com");
+		user.setPhoneNumber(BigInteger.valueOf(8749327930L));
+		
+		System.out.println(user);
+		
+		Booking book=new Booking();
+		book.setUserId(user.getUserId());
+		book.setDateOfJourney(LocalDate.now());
+		List<Passenger> passList=new ArrayList<Passenger>();
+		Passenger p=new Passenger();
+		p.setPassengerName("rtueo");
+		p.setPassengerAge(20);
+		p.setPassengerGender('F');
+		p=userdao.savePassenger(p);
+		System.out.println(p);
+		passList.add(p);
+		book.setPassengers(passList);
+		book.setModeOfPayment("UPI");
+		book.setBookingStatus("booked");
+		book.setBus(b);
+		book.setTotalCost(b.getCost());
+		
+		System.out.println(userdao.saveBooking(book));
+		
 		
 	}
 
