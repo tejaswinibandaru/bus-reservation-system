060cc2fa37f637582c48a3d03f2852bc32161b7f
Bandaru Tejaswini <bandaruteju@gmail.com>
2019-09-16 17:51:03 +0530

    Test class added

---
+++ Sprint 2/BusReservationSystem/src/test/java/com/cg/busreservationsystem/test/SystemTest.java
@@ -0,0 +1,26 @@
+package com.cg.busreservationsystem.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SystemTest {
+
+	@BeforeEach
+	void beforeEachTest() {
+		
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@AfterEach
+	void afterEachTest() {
+		
+	}
+
+}
